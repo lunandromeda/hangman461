af4402f694df85e38bf3fc57d540f8e7ae311241
lunandromeda <lauren2.salmon@live.uwe.ac.uk>
2024-10-13 19:48:56 +0100

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,17 +1,28 @@
-# Task 1
-favourite_fruits = ['banana', 'strawberry', 'mango', 'watermelon', 'grapes']
-word_list = favourite_fruits
-print(word_list)
-
-# Task 2
 import random
 
-word = random.choice(word_list)
-print(word)
+# Task 1: Create list of favourite fruits
+def get_favourite_fruits():
+    return ['banana', 'strawberry', 'mango', 'watermelon', 'grapes']
 
-# Task 3
-guess = input("Please enter a single letter: ")
-while len(guess) != 1 or not guess.isalpha():
-          guess = input("Invalid input. Please enter a single letter: ")
+# Task 2: Select a random fruit from the list
+def choose_random_fruit(fruit_list):
+    return random.choice(fruit_list)
 
-print(f"You entered: {guess}")
+# Task 3: Prompt user to guess a letter
+def get_user_guess():
+    guess = input("Guess a letter: ")
+    while len(guess) != 1 or not guess.isalpha():
+        guess = input("Invalid input. Please enter a single letter: ")  
+    return guess
+
+# Main function
+if __name__ == "__main__":
+    #Task 1
+    fruit_list = get_favourite_fruits()
+    print("Favourite fruits: ", fruit_list)
+    #Task 2
+    random_fruit = choose_random_fruit(fruit_list)
+    print("Random fruit selected: ", random_fruit)
+    #Task 3
+    user_guess = get_user_guess()
+    print(f"You entered: {user_guess}")
