c7fbc9499eecd1093d7cd882ef59b364b1f487fb
lunandromeda <lauren2.salmon@live.uwe.ac.uk>
2024-10-13 20:11:21 +0100

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,24 @@
+secret_word = 'apple' # The word that needs to be guessed
+
 while True: # Continuous Loop
-    # Step 2: Ask user to guess a letter
+    # Ask user to guess a letter
     guess = input("Guess a letter: ")
 
-    # Step 3: Check if the input is a single letter
+    # Check if the input is a single letter
     if len(guess) == 1 and guess.isalpha():
-        # Step 4: If the input is a single letter, print the letter
+        # If the input is a single letter, print the letter
         break
     else:
-        #Step 5: If the input is not a single letter, print an error message
+        # If the input is not a single letter, print an error message
         print("Invalid letter. Please, enter a single alphabetical character.")
 
 # The loop will continue to run until the user enters a single letter
 print(f"You enetered a valid letter: {guess}")
+
+# Check if the guessed letter is in the secret word
+if guess in secret_word:
+    # If true, print a success message
+    print(f"Good guess! {guess} is in the word.")
+else:
+    # If false, print a failure message
+    print(f"Sorry, {guess} is not in the word. Try again.")
