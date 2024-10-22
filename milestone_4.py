3835544c690a1ee0067b0d3cecae63c009e55244
lunandromeda <lauren2.salmon@live.uwe.ac.uk>
2024-10-22 19:29:45 +0100

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -12,7 +12,7 @@ class Hangman:
 
     # Method to check if guessed letter is in the word
     def check_guess(self, guess):
-        guess = guess.lower()  # Convert guess to lowercase
+        guess = guess.lower()  # Convert guess to lowercaseb
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             # For loop to replace underscores with correct guessed letter
@@ -25,7 +25,7 @@ class Hangman:
         else:
             print(f"Sorry, {guess} is not in the word.")
             self.num_lives -= 1  # Decrease life for evry incorrect guess
-            print(f"Number of lives left: {self.num_lives}")
+            print(f"You have number of lives left: {self.num_lives}")
     
     # Method to ask for input from user
     def ask_for_input(self):
