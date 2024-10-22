8baeb4ae1bbe67f4d7c3a987ff66e4e39b759d18
lunandromeda <lauren2.salmon@live.uwe.ac.uk>
2024-10-22 19:22:38 +0100

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,10 +1,7 @@
 import random
 
-# Step 1: Define the Hangman class
 class Hangman:
-# Step 2: Define the __init__ method
     def __init__(self, word_list, num_lives=5):
-        # Step 3: Initialise the attributes
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
@@ -12,13 +9,37 @@ class Hangman:
         self.num_letters = len(set(self.word))  # Unique letters to be guessed
         self.list_of_guesses = []
 
+    # Step 1: Method to check if the guessed letter is in word
+    def check_guess(self, guess):
+        guess = guess.lower()  # Converts the guess to lowercase
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word.")
+            self.num_lives -= 1  # Decrease life for every incorrect guess
+            print(f"Number of lives left: {self.num_lives}")
+    
+    # Step 2: Method to ask for input from the user
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter: ")
+
+            # Check if guess is a single alphabetical character
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            # Check if the letter was already guessed
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            # Valid guess
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)  # Appends guess to the list of guesses
+                break  # Exit the loop after a valid guess is made
+
 # Trying it out
 if __name__ == "__main__":
     word_list = ['banana', 'strawberry', 'mango', 'watermelon', 'grapes']
     game = Hangman(word_list)
     
-    print(f"Word to guess: {game.word}")
-    print(f"Word guessed so far: {game.word_guessed}")
-    print(f"Number of unique letters: {game.num_letters}")
-    print(f"Number of lives: {game.num_lives}")
-    print(f"List of guesses: {game.list_of_guesses}")
+    print(f"The word is: {''.join(game.word_guessed)}")
+    game.ask_for_input()
